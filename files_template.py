5678d3788f10fe9a9e4ac529e68a0c5669eaa495
Nathan Wandji <nathan-wilfried.wandji-tabeko@efrei.net>
2026-01-23 22:06:16 +0100

    Initialization DONE

---
+++ files_template.py
@@ -0,0 +1,52 @@
+# Code python qui va me permettre de setup de façon automatique tous les folders et fichiers nécessaire pour le projet 
+
+import os 
+from pathlib import Path 
+import logging 
+
+# Logging string
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
+
+project_name = 'cnnClassifier'
+
+list_of_files = [
+    ".github/workflows/.gitkeep",
+    # Dans le folder "src", sera stocké tou les composants de notre app 
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/constants/__init__.py",
+    "config/config.yaml",
+    "dvc.yaml",
+    "params.yaml",
+    "requirements.txt",
+    "setup.py",
+    "research/trials.ipynb",
+    "templates/index.html"
+]
+
+
+for filepath in list_of_files:
+
+    # Création du path pour chaque fichier
+    filepath = Path(filepath)
+    # Pour séparer les paths de dossiers et ceux de fichiers 
+    filedir, filename = os.path.split(filepath)
+
+
+    if filedir !="":
+        os.makedirs(filedir, exist_ok=True)
+        logging.info(f"Creating directory; {filedir} for the file: {filename}")
+
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
+        with open(filepath, "w") as f:
+            pass
+            logging.info(f"Creating empty file: {filepath}")
+
+
+    else:
+        logging.info(f"{filename} is already exists")
