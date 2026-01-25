eccdacb927e16e7bf0d6c27eef48aa071c5d59a0
Nathan Wandji <nathan-wilfried.wandji-tabeko@efrei.net>
2026-01-25 12:58:38 +0100

    prepare base model completed

--- src/cnnClassifier/config/configuration.py
+++ src/cnnClassifier/config/configuration.py
@@ -1,7 +1,8 @@
 from src.cnnClassifier.constants import *
 import os
 from src.cnnClassifier.utils.common import read_yaml, create_directories,save_json
-from src.cnnClassifier.entity.config_entity import DataIngestionConfig
+from src.cnnClassifier.entity.config_entity import (DataIngestionConfig, 
+                                                    PrepareBaseModelConfig)
 
 
 class ConfigurationManager:
@@ -17,7 +18,7 @@ class ConfigurationManager:
 
 
     
-    # Pour récupérer les paramètres d'ingestion des données 
+    # Pour récupérer les configs d'ingestion des données 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config = self.config.data_ingestion
 
@@ -31,3 +32,26 @@ class ConfigurationManager:
         )
 
         return data_ingestion_config
+    
+
+    # Pour récupérer les configs et paramètres du modèle 
+    def get_prepare_base_model_config(self) -> PrepareBaseModelConfig:
+        config = self.config.prepare_base_model
+        
+        create_directories([config.root_dir])
+
+        prepare_base_model_config = PrepareBaseModelConfig(
+            # configs 
+            root_dir=Path(config.root_dir),
+            base_model_path=Path(config.base_model_path),
+            updated_base_model_path=Path(config.updated_base_model_path),
+
+            # Params 
+            params_image_size=self.params.IMAGE_SIZE,
+            params_learning_rate=self.params.LEARNING_RATE,
+            params_include_top=self.params.INCLUDE_TOP,
+            params_weights=self.params.WEIGHTS,
+            params_classes=self.params.CLASSES
+        )
+
+        return prepare_base_model_config
