d66bb5ead1363fc602d90494c8c0b27d04b57c23
Nathan Wandji <nathan-wilfried.wandji-tabeko@efrei.net>
2026-01-27 13:39:57 +0100

    prediction using flask completed

---
+++ app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, jsonify, render_template
+import os
+from flask_cors import CORS, cross_origin
+from src.cnnClassifier.utils.common import decodeImage
+from src.cnnClassifier.pipeline.prediction import PredictionPipeline
+
+
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+
+class ClientApp:
+    def __init__(self):
+        # Initialise le fichier d’entrée et le pipeline de prédiction
+        self.filename = "inputImage.jpg"
+        self.classifier = PredictionPipeline(self.filename)
+
+
+@app.route("/", methods=['GET'])
+@cross_origin()
+def home():
+    # Affiche la page d’accueil
+    return render_template('index.html')
+
+
+
+
+@app.route("/train", methods=['GET','POST'])
+@cross_origin()
+def trainRoute():
+    # Lance l’entraînement du modèle
+    # os.system("python main.py")
+    os.system("dvc repro") # A utiliser si je veux tracker la pipeline 
+    return "Training done successfully!"
+
+
+
+@app.route("/predict", methods=['POST'])
+@cross_origin()
+def predictRoute():
+    # Retourne la prédiction du modèle
+    image = request.json['image']
+    decodeImage(image, clApp.filename)
+    result = clApp.classifier.predict()
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    # Pour démarrer l'application flask 
+    clApp = ClientApp()
+    app.run(host='0.0.0.0', port=8080) #for AWS
+
